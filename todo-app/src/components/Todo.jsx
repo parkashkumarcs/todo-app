b351ac14ba15d0001ff5e6926ad8e3a665338bc8
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-08-08 00:16:39 +0500

    useState logic added to add task by events handling and updated7 on 08/08/2025

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,17 +1,28 @@
 // Importing the react libraries, css files, and other components
 
 import './todoStyle.css'
-import { useState } from 'react';
+import { use, useState } from 'react';
 
 const Todo = () => {
+    const [todoList, setTodoList] = useState([]);
     const [inputValue, setInput] = useState('');
+    
+    const handleClick = e => {
+        e.preventDefault();
+
+        if(inputValue.trim()){
+            setTodoList([...todoList, inputValue]);
+            setInput('');
+        }
+
+    }
   return (
     <div className="todo__Container">
       <h1 className="heading__1">
         T o<span className="do"> D o</span>
       </h1>
       <p className="faq__title">What's the plan for today?</p>
-      <div className="input__Container">
+      <div className="input__Container" >
         <input 
             type="text" 
             placeholder='Write your new task?'
@@ -19,11 +30,18 @@ const Todo = () => {
         
         />
         <span>
-            <button>Add Task</button>
+            <button onClick={handleClick}>Add Task</button>
         </span>
       </div>
+
+
       <div className="display__Todo-list">
         <ul>
+
+            {todoList.map((todo, index) =>(
+                <li key={index}>{todo}</li>
+            ))}
+            {/* <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
@@ -45,8 +63,7 @@ const Todo = () => {
             <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
-            <li>Just Sample of todo list diplay</li>
-            <li>Just Sample of todo list diplay</li>
+            <li>Just Sample of todo list diplay</li> */}
         </ul>
       </div>
     </div>

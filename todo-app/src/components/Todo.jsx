ad49780689b63355b67ed3d9477594cb991aba0f
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-08-07 23:16:34 +0500

    todo__Container has styled, css lines of code added and updated4 on 08/07/2025

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -6,11 +6,15 @@ const Todo = () => {
   return (
     <div className="todo__Container">
       <h1 className="heading__1">
-        To<span className="do">Do</span>
+        T o<span className="do"> D o</span>
       </h1>
       <p className="faq__title">What's the plan for today?</p>
       <div className="input__Container">
-        <input type="text" />
+        <input 
+            type="text" 
+            placeholder='Write your new task?'
+        
+        />
         <span>
             <button>Add Task</button>
         </span>

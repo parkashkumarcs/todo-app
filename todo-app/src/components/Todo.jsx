34c6ac513d23d69585a8031207408393d8505f47
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-08-09 21:50:35 +0500

    Favicon for remove, edit icons created, css properties added for icons and updated on 08/9/2025

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,47 +1,54 @@
 // Importing the react libraries, css files, and other components
 
-import './todoStyle.css'
-import { use, useState } from 'react';
+import "./todoStyle.css";
+import { use, useState } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 const Todo = () => {
-    const [todoList, setTodoList] = useState([]);
-    const [inputValue, setInput] = useState('');
-    
-    const handleClick = e => {
-        e.preventDefault();
+  const [todoList, setTodoList] = useState([]);
+  const [inputValue, setInput] = useState("");
 
-        if(inputValue.trim()){
-            setTodoList([...todoList, inputValue]);
-            setInput('');
-        }
+  const handleClick = (e) => {
+    e.preventDefault();
 
+    if (inputValue.trim()) {
+      setTodoList([...todoList, inputValue]);
+      setInput("");
     }
+  };
   return (
     <div className="todo__Container">
       <h1 className="heading__1">
         T o<span className="do"> D o</span>
       </h1>
       <p className="faq__title">What's the plan for today?</p>
-      <div className="input__Container" >
-        <input 
-            type="text" 
-            placeholder='Write your new task?'
-            onChange={e => setInput(e.target.value)}
-        
+      <div className="input__Container">
+        <input
+          type="text"
+          placeholder="Write your new task?"
+          onChange={(e) => setInput(e.target.value)}
         />
         <span>
-            <button onClick={handleClick}>Add Task</button>
+          <button onClick={handleClick}>Add Task</button>
         </span>
       </div>
 
-
       <div className="display__Todo-list">
         <ul>
-
-            {todoList.map((todo, index) =>(
-                <li key={index}>{todo}</li>
-            ))}
-            {/* <li>Just Sample of todo list diplay</li>
+          {todoList.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <span className="right__Align">
+                <span className="remove__Icon">
+                  <FaTrash />
+                </span>
+                <span className="edit__Icon">
+                  <FaEdit />
+                </span>
+              </span>
+            </li>
+          ))}
+          {/* <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
             <li>Just Sample of todo list diplay</li>
